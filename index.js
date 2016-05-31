f0e8c9fbf280441b3809af7dbe3a259256af2ca7
Pratinav Bagla <pratinavbagla@gmail.com>
2016-05-31 13:48:19 +0530

    Changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.argv[2] || 3000;
+var port = process.argv[2] || 80;
 
 // Routing
 app.use(express.static(__dirname + '/public'));
